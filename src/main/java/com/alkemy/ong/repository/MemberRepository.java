a8001c332712796fc337abb2b5bee06815f388ac
Lucas López <lucaslopezd@hotmail.com>
2022-08-18 08:37:44 -0300

    repository Member added #OT278-23

--- src/main/java/com/alkemy/ong/repository/MemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.alkemy.ong.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alkemy.ong.model.Member;
-
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long>{
     
 }
