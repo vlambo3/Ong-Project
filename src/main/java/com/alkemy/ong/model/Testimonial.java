8ec1190587e7274875e2a32bd391b216d87fdd54
Federico Carrasco <federicogcarrasco@gmail.com>
2022-08-18 16:24:17 -0300

    Add Testimonial model and repository

---
+++ src/main/java/com/alkemy/ong/model/Testimonial.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "testimonials")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE testimonials SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
+public class Testimonial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    private String image;
+
+    private String content;
+
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    private boolean deleted = Boolean.FALSE;
+
+}
---
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.model.Testimonial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestimonialRepository extends JpaRepository<Testimonial, Long> {
+
+}
