5bf5a33af11964524b90c2e9852a0b51d6291c69
matiasnb28 <matiasnb28@gmail.com>
2022-08-19 16:01:43 -0300

    User model in progress

---
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @Column(unique = true)
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+
+    private String photo;
+
+    private Long roleId;
+    private boolean deleted = Boolean.FALSE;
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDateTime creationDate;
+
+
+
+}
