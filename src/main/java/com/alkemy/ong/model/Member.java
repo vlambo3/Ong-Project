d64584a03eaef2cebc65d99ef9d9c9e627ef8449
Lucas López <lucaslopezd@hotmail.com>
2022-08-19 09:46:16 -0300

    Model Member / error correction - OT278-23

--- src/main/java/com/alkemy/ong/model/Member.java
+++ src/main/java/com/alkemy/ong/model/Member.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.model;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,8 +9,11 @@ import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,7 +22,8 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "members")
-@SQLDelete(sql = "UPDATE members SET member_deleted = TRUE WHERE member_id = ?")
+@SQLDelete(sql = "UPDATE members SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -28,30 +32,31 @@ public class Member {
     
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "member_id")
     private Long id;
 
-    @Column(name = "member_name", length = 35, nullable = false)
+    @NotEmpty(message = "Name may not be null or empty")
+    @Column(length = 35, nullable = false)
     private String name;
 
-    @Column(name = "member_facebook_url", length = 100, nullable = true)
+    @Column(name = "facebook_url", length = 100, nullable = true)
     private String facebookUrl;
 
-    @Column(name = "member_instagram_url", length = 100, nullable = true)
+    @Column(name = "instagram_url", length = 100, nullable = true)
     private String instagramUrl;
 
-    @Column(name = "member_linkedin_url", length = 100, nullable = true)
+    @Column(name = "linkedin_url", length = 100, nullable = true)
     private String linkedinUrl;
 
-    @Column(name = "member_image", length = 100, nullable = false)
+    @NotNull(message = "Image may not be null")
+    @Column(length = 100, nullable = false)
     private String image;
 
-    @Column(name = "member_description", nullable = true)
+    @Column(nullable = true)
     private String description;
 
-    @Column(name = "member_creation_date", nullable = false)
-    private Timestamp creationDate;
+    @Column(name = "creation_date", nullable = false)
+    private LocalDateTime creationDate;
     
-    @Column(name = "member_deleted", nullable = false)
+    @Column(nullable = false)
     private Boolean deleted;
 }
