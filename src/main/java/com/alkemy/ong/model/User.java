ee1c410b633b788ec586e2c8c406bedfe182027e
matiasnb28 <matiasnb28@gmail.com>
2022-08-22 19:05:51 -0300

    correcting errors

--- src/main/java/com/alkemy/ong/model/User.java
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
-import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -25,22 +27,37 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
+
+    @NotNull(message = "FirstName cannot be null.")
+    @Column(name = "first_name")
     private String firstName;
-    @NotNull
+
+    @NotNull(message = "LastName cannot be null.")
+    @Column(name = "last_name")
     private String lastName;
+
     @Column(unique = true)
-    @NotNull
+    @NotNull(message = "Email cannot be null.")
+    @Email
     private String email;
-    @NotNull
+
+    @NotNull(message = "Password cannot be null.")
     private String password;
 
     private String photo;
 
-    private Long roleId;
+    @OneToOne
+    private Role role;
+
     private boolean deleted = Boolean.FALSE;
-    @DateTimeFormat(pattern = "yyyy/MM/dd")
-    private LocalDateTime creationDate;
+
+    @CreationTimestamp
+    @Column(name = "creation_date", updatable = false)
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
 
 
 
