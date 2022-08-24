8804826f6a02ec688e95d8866a406a6c93188df5
Federico Carrasco <federicogcarrasco@gmail.com>
2022-08-24 01:55:23 -0300

    add UserRepository import in UserService

--- src/main/java/com/alkemy/ong/security/service/UserService.java
+++ src/main/java/com/alkemy/ong/security/service/UserService.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.security.service;
 
 import com.alkemy.ong.model.User;
+import com.alkemy.ong.repository.UserRepository;
 import com.alkemy.ong.security.dto.UserRequestDto;
 import com.alkemy.ong.security.dto.UserResponseDto;
 import com.alkemy.ong.security.mapper.UserMapper;
