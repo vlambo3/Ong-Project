cea2fca47a4e7d463661ddfd0a049dd958a9ab4b
Vanina Godoy <contadora.godoy.vanina@gmail.com>
2022-08-24 13:56:46 -0300

    add autowired

--- src/main/java/com/alkemy/ong/security/controller/UserController.java
+++ src/main/java/com/alkemy/ong/security/controller/UserController.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.security.controller;
 import com.alkemy.ong.security.dto.AuthenticationRequest;
 import com.alkemy.ong.security.dto.AuthenticationResponse;
 import com.alkemy.ong.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @PostMapping("/login")
