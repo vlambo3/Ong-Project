2c84d2eaf03b417d94b428162ce56e4fd4d5a0dc
Damian Pereyra <pereyra.damian89@gmail.com>
2022-08-25 18:14:34 -0300

    changes Category.java

--- src/main/java/com/alkemy/ong/model/Category.java
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -2,18 +2,23 @@ package com.alkemy.ong.model;
 
 
 
-import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
+
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+
 
 @Entity
 @Table(name = "categories")
@@ -27,14 +32,22 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotNull
+
+    @NotNull(message = "Name cannot be empty")
     private String name;
 
     private String description;
 
     private String image;
 
-    private LocalDateTime creationDate = LocalDateTime.now();
+
+    @CreationTimestamp
+    private Timestamp creationTimestamp;
+
+    @UpdateTimestamp
+    private Timestamp UpdateTimeStamp;
+
+
 
     private boolean deleted = Boolean.FALSE;
 
