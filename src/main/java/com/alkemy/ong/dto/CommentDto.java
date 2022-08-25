95a2822cffadd78889c9abc0fae7f2d24764c769
matiasnb28 <matiasnb28@gmail.com>
2022-08-25 12:29:24 -0300

    adding annotation to dto

--- src/main/java/com/alkemy/ong/dto/CommentDto.java
+++ src/main/java/com/alkemy/ong/dto/CommentDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 
@@ -16,6 +17,7 @@ public class CommentDto implements Serializable {
 
     private Long id;
 
+    @NotNull(message = "Body can't be null.")
     private String body;
 
     private Long user_id;
