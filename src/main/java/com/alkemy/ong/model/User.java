708367eaf23c317c0821ee415d19bb44da89baf5
Vanina Godoy <contadora.godoy.vanina@gmail.com>
2022-08-27 12:28:24 -0300

    Change long --> Long

--- src/main/java/com/alkemy/ong/model/User.java
+++ src/main/java/com/alkemy/ong/model/User.java
@@ -8,7 +8,6 @@ import org.hibernate.annotations.Where;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 @Entity
