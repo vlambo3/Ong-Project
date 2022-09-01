66719a1ebae979e592cf27d527a8b36fa77dc915
Federico Carrasco <federicogcarrasco@gmail.com>
2022-09-01 17:05:09 -0300

    Add custom exception to Contact save

---
+++ src/main/java/com/alkemy/ong/exception/UnableToSaveEntityException.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.exception;
+
+public class UnableToSaveEntityException extends RuntimeException {
+
+    public UnableToSaveEntityException(String message) {
+        super(message);
+    }
+
+}
