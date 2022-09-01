5623e95bf47fa3c969cc8097b77d164a490ac740
Federico Carrasco <federicogcarrasco@gmail.com>
2022-09-01 17:04:31 -0300

    Add try catch with custom exception to Contact save

--- src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
@@ -1,12 +1,16 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.ContactDto;
+import com.alkemy.ong.exception.UnableToSaveEntityException;
 import com.alkemy.ong.mapper.ContactMapper;
 import com.alkemy.ong.model.Contact;
 import com.alkemy.ong.repository.ContactRepository;
 import com.alkemy.ong.service.IContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +18,16 @@ public class ContactServiceImpl implements IContactService {
 
     private final ContactMapper mapper;
     private final ContactRepository repository;
+    private final MessageSource messageSource;
 
     public ContactDto save(ContactDto dto) {
-        Contact entity = mapper.contactDto2ContactEntity(dto);
-        Contact savedEntity = repository.save(entity);
-        return mapper.contactEntity2ContactDto(savedEntity);
+        try {
+            Contact entity = mapper.contactDto2ContactEntity(dto);
+            Contact savedEntity = repository.save(entity);
+            return mapper.contactEntity2ContactDto(savedEntity);
+        } catch (Exception e) {
+            throw new UnableToSaveEntityException(messageSource.getMessage("unable-to-save-entity", null, Locale.US));
+        }
     }
 
 }
