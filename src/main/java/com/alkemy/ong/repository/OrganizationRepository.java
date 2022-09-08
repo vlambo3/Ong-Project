4c331da4048aecce7f64b1619028c17e378c9212
LucasLopezd <96482300+LucasLopezd@users.noreply.github.com>
2022-09-08 12:12:40 -0300

    Revert "Social media ot278 64 (#60)"
    This reverts commit 5b9a75de00dd52348fc328f2e10f4d9b2c97b48c.

--- src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
@@ -1,5 +1,7 @@
 package com.alkemy.ong.repository;
 
+
+
 import com.alkemy.ong.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
