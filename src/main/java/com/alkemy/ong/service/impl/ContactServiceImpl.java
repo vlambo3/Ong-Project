f02fc629a244d8f63f5c7b139855d005858b650a
Federico Carrasco <federicogcarrasco@gmail.com>
2022-09-13 15:44:49 -0300

    Change Contact Mapper

--- src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
@@ -4,7 +4,7 @@ import com.alkemy.ong.exception.EmptyListException;
 import com.alkemy.ong.dto.contact.ContactRequestDto;
 import com.alkemy.ong.dto.contact.ContactResponseDto;
 import com.alkemy.ong.exception.UnableToSaveEntityException;
-import com.alkemy.ong.mapper.ContactMapper;
+import com.alkemy.ong.mapper.GenericMapper;
 import com.alkemy.ong.model.Contact;
 import com.alkemy.ong.service.IContactService;
 import com.alkemy.ong.service.IEmailService;
@@ -21,17 +21,17 @@ import java.util.Locale;
 @RequiredArgsConstructor
 public class ContactServiceImpl implements IContactService {
 
-    private final ContactMapper mapper;
+    private final GenericMapper mapper;
     private final ContactRepository repository;
     private final IEmailService emailService;
     private final MessageSource messageSource;
 
     public ContactResponseDto save(ContactRequestDto dto) {
         try {
-            Contact entity = mapper.contactDto2ContactEntity(dto);
+            Contact entity = mapper.map(dto, Contact.class);
             Contact savedEntity = repository.save(entity);
             emailService.sendEmail(savedEntity.getEmail());
-            return mapper.contactEntity2ContactDto(savedEntity);
+            return mapper.map(savedEntity, ContactResponseDto.class);
         } catch (Exception e) {
             throw new UnableToSaveEntityException(messageSource.getMessage("unable-to-save-entity", null, Locale.US));
         }
@@ -42,6 +42,6 @@ public class ContactServiceImpl implements IContactService {
         if (contacts.isEmpty()) {
             throw new EmptyListException(messageSource.getMessage("empty-list", null, Locale.US));
         }
-        return mapper.contactEntityList2ContactDtoList(contacts);
+        return mapper.mapAll(contacts, ContactResponseDto.class);
     }
 }
