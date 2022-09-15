84adbde1b0af916f11a35b5d6952f2d6edcd6ad9
Federico Carrasco <federicogcarrasco@gmail.com>
2022-09-15 13:46:53 -0300

    Use String.format() to create links

--- src/main/java/com/alkemy/ong/mapper/GenericMapper.java
+++ src/main/java/com/alkemy/ong/mapper/GenericMapper.java
@@ -18,10 +18,10 @@ public class GenericMapper implements Serializable {
 
     private final ModelMapper mapper;
 
-    @Value("${alkemy.pageLink}")
-    private String pageLink;
+    @Value("${alkemy.pageQueryLink}")
+    private String pageQueryLink;
 
-    private String queryParam = "?page=";
+    //private String queryParam = "?page=";
 
     public <S, D> D map(S source, Class<D> destinationClass) {
         return mapper.map(source, destinationClass);
@@ -36,9 +36,9 @@ public class GenericMapper implements Serializable {
     public <P, D> PageDto<D> mapPage(Page<P> page, Class<D> destinationClass, String classPath) {
         PageDto<D> dto = new PageDto<>();
         if (!page.isFirst())
-            dto.setPreviousPage(pageLink + classPath + queryParam + (page.getNumber() - 1));
+            dto.setPreviousPage(String.format(pageQueryLink, classPath) + (page.getNumber() - 1));
         if (!page.isLast())
-            dto.setNextPage(pageLink + classPath + queryParam + (page.getNumber() + 1));
+            dto.setNextPage(String.format(pageQueryLink, classPath) + (page.getNumber() + 1));
         dto.setContent(mapAll(page.getContent(), destinationClass));
         return dto;
     }
