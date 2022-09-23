20dabb7c30021c654e7db13cdcdc482ac76f7f22
matiasnb28 <matiasnb28@gmail.com>
2022-09-22 22:05:38 -0300

    Starting test

---
+++ src/test/java/com/alkemy/ong/endpoints/integration/controller/category/CategoryControllerTest.java
@@ -0,0 +1,80 @@
+package com.alkemy.ong.endpoints.integration.controller.category;
+
+
+import com.alkemy.ong.dto.category.CategoryRequestDto;
+import com.alkemy.ong.dto.category.CategoryResponseDto;
+import com.alkemy.ong.mapper.GenericMapper;
+import com.alkemy.ong.service.ICategoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestPropertySource(locations = "")
+public class CategoryControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    protected MockMvc mockMvc;
+
+    @MockBean
+    private ICategoryService service;
+    @Autowired
+    private GenericMapper mapper;
+    @Autowired
+    private ObjectMapper objectMapper;
+    private CategoryRequestDto categoryDto;
+
+    List<CategoryRequestDto> categoryDtoList = new ArrayList<>();
+
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+        categoryDto = new CategoryRequestDto();
+        categoryDto.setName("name");
+        categoryDto.setDescription("9876543210");
+        categoryDto.setImage("otherexample@gmail.com");
+
+
+        categoryDtoList.add(categoryDto);
+    }
+
+
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void createCategoryAdmin201() throws Exception {
+        when(service.create(categoryDto)).thenReturn(mapper.map(categoryDto, CategoryResponseDto.class));
+        mockMvc.perform(post("/categories")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(categoryDto))
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf()))
+                .andExpect(status().isCreated());
+    }
+}
