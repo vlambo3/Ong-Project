c2843335c6937c4ce1c0f2ffee4b8b3ab2b923d7
Gian <98044720+GianBarritta@users.noreply.github.com>
2022-09-23 17:55:40 -0300

    Add news documentation interface (#110)

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -1,20 +1,15 @@
 package com.alkemy.ong.controller;
 
 
+import com.alkemy.ong.dto.PageDto;
 import com.alkemy.ong.dto.comment.CommentBasicResponseDto;
 import com.alkemy.ong.dto.news.NewsRequestDto;
 import com.alkemy.ong.dto.news.NewsResponseDto;
-import com.alkemy.ong.exception.CustomExceptionDetails;
 import com.alkemy.ong.service.ICommentService;
-import com.alkemy.ong.dto.PageDto;
 import com.alkemy.ong.service.INewsService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import com.alkemy.ong.utils.documentation.INewsController;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,27 +17,20 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@RequiredArgsConstructor
-@RequestMapping("/news")
 @RestController
-public class NewsController {
+@RequestMapping("/news")
+@RequiredArgsConstructor
+@Api(value = "News controller", description = "This controller has a CRUD for news")
+public class NewsController implements INewsController {
 
     private final INewsService service;
     private final ICommentService commentService;
 
+    @PostMapping
+    public ResponseEntity<NewsResponseDto> create(@Valid @RequestBody NewsRequestDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
+    }
 
-    @Operation(summary = "Get New by id.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "New found.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = NewsResponseDto.class))}),
-            @ApiResponse(responseCode = "403", description = "Invalid token or token expired | Accessing with invalid role.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))}),
-            @ApiResponse(responseCode = "404", description = "New not found.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))})
-    })
     @GetMapping("/{id}")
     public ResponseEntity<NewsResponseDto> getById(@PathVariable Long id)  {
         return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
@@ -54,66 +42,20 @@ public class NewsController {
         return ResponseEntity.ok(pageDto);
     }
 
-    @Operation(summary = "Create New.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Created New.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = NewsResponseDto.class))}),
-            @ApiResponse(responseCode = "400", description = "Field cannot be empty.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))}),
-            @ApiResponse(responseCode = "403", description = "Invalid token or token expired | Accessing with invalid role.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))}),
-            @ApiResponse(responseCode = "404", description = "New not found.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))})
-    })
-    @PostMapping
-    public ResponseEntity<NewsResponseDto> createNew(@Valid @RequestBody NewsRequestDto dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
-    }
-
-    @Operation(summary = "Delete new.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "New has been deleted.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = NewsResponseDto.class))}),
-            @ApiResponse(responseCode = "403", description = "Invalid token or token expired | Accessing with invalid role.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))}),
-            @ApiResponse(responseCode = "404", description = "New not found.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))})
-    })
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteNews(@PathVariable Long id) {
-        service.delete(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @Operation(summary = "Update new by id.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Updated new.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = NewsResponseDto.class))}),
-            @ApiResponse(responseCode = "403", description = "Invalid token or token expired | Accessing with invalid role.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))}),
-            @ApiResponse(responseCode = "404", description = "New not found.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomExceptionDetails.class))})
-    })
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@Valid @RequestBody NewsRequestDto news, @PathVariable Long id) {
-        NewsResponseDto newResponse = service.update(news, id);
-        return new ResponseEntity<>(newResponse, HttpStatus.CREATED);
-    }
-
     @GetMapping("/{id}/comments")
     public ResponseEntity<List<CommentBasicResponseDto>> getCommentsByNewsId(@Valid @PathVariable Long id){
         List<CommentBasicResponseDto> comments = commentService.getAllCommentsByNewsId(id);
         return ResponseEntity.status(HttpStatus.OK).body(comments);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<NewsResponseDto> update(@Valid @PathVariable Long id, @RequestBody NewsRequestDto news) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(news, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
