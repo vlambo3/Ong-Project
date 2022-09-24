4c0df66947a1f3ba2d0105306b1c3271d402ef5f
Federico Carrasco <federicogcarrasco@gmail.com>
2022-09-24 13:34:57 -0300

    fix comments delete

--- src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
@@ -4,12 +4,8 @@ package com.alkemy.ong.service.impl;
 import com.alkemy.ong.dto.comment.CommentBasicResponseDto;
 import com.alkemy.ong.dto.comment.CommentRequestDto;
 import com.alkemy.ong.dto.comment.CommentResponseDto;
-import com.alkemy.ong.exception.EmptyListException;
-import com.alkemy.ong.exception.IdNullOrNegativeException;
-import com.alkemy.ong.exception.NotFoundException;
+import com.alkemy.ong.exception.*;
 import com.alkemy.ong.dto.comment.CommentBodyResponseDto;
-import com.alkemy.ong.exception.ForbiddenException;
-import com.alkemy.ong.exception.UnableToUpdateEntityException;
 import com.alkemy.ong.mapper.GenericMapper;
 import com.alkemy.ong.model.Comment;
 import com.alkemy.ong.security.auth.UserService;
@@ -17,6 +13,7 @@ import com.alkemy.ong.security.dto.UserResponseDto;
 import com.alkemy.ong.security.jwt.JwtUtils;
 import com.alkemy.ong.service.ICommentService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import com.alkemy.ong.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
@@ -34,13 +31,8 @@ public class CommentServiceImpl implements ICommentService {
     private final CommentRepository repository;
     private final GenericMapper mapper;
     private final MessageSource messageSource;
-
-
-
-
     @Autowired
     private JwtUtils jwtUtils;
-
     @Autowired
     private UserService userService;
 
@@ -48,28 +40,8 @@ public class CommentServiceImpl implements ICommentService {
     public CommentResponseDto save(CommentRequestDto commentRequestDto) {
         Comment comment = mapper.map(commentRequestDto, Comment.class);
         Comment savedComment = repository.save(comment);
-
         return mapper.map(savedComment, CommentResponseDto.class);
-
-
     }
-
-
-    @Override
-
-    public void delete(Authentication aut, Long id) {
-        try {
-            if (checkId(aut, id)) {
-                Comment entity = repository.getById(id);
-                entity.setDeleted(true);
-                repository.save(entity);
-            }
-        }catch (Exception e){
-            throw new NullPointerException(messageSource.getMessage("comment.not.found", null, Locale.US));
-
-        }
-    }
-
 
     @Override
     public List<CommentBasicResponseDto> getAllCommentsByNewsId(Long newsId) {
@@ -83,7 +55,52 @@ public class CommentServiceImpl implements ICommentService {
         return mapper.mapAll(comments, CommentBasicResponseDto.class);
     }
 
+    @Override
+    public List<CommentBodyResponseDto> getAllCommentBodies() {
+        List<Comment> bodies = repository.findAllByOrderByNewsCreationDateAsc();
+        if(bodies.isEmpty()){
+            throw new EmptyListException(messageSource.getMessage("empty-list", null, Locale.US));
+        }
+        return mapper.mapAll(bodies, CommentBodyResponseDto.class);
+    }
 
+    @Override
+    public CommentResponseDto update(Long id, CommentRequestDto edit,String auth) {
+        Comment comment = getCommentById(id);
+        UserResponseDto user = userService.getLoggerUserData(auth);
+        String userRole = user.getRole().getName().name();
+        if (userRole != "ADMIN" && user.getId() != comment.getUserId())
+            throw new ForbiddenException(messageSource.getMessage("forbidden",null,Locale.US));
+        try{
+            comment.setBody(edit.getBody());
+            comment.setUserId(edit.getUserId());
+            comment.setNewsId(edit.getNewsId());
+            comment.setUpdateDate(LocalDateTime.now());
+            repository.save(comment);
+            return mapper.map(comment, CommentResponseDto.class);
+        }catch (Exception e){
+            throw new UnableToUpdateEntityException(messageSource.getMessage("unable-to-update-comment",
+                    null, Locale.US));
+        }
+    }
+
+    @Override
+    public void delete(Authentication aut, Long id) {
+        Comment comment = getCommentById(id);
+        try {
+            comment.setUpdateDate(LocalDateTime.now());
+            repository.deleteById(id);
+        }catch (Exception e){
+            throw new UnableToDeleteEntityException(messageSource.getMessage("unable-to-delete-comment", new Object[] {id}, Locale.US));
+        }
+    }
+
+    private Comment getCommentById(Long id) {
+        Optional<Comment> comment = repository.findById(id);
+        if(comment.isEmpty())
+            throw new NotFoundException(messageSource.getMessage("comment-not-found", null, Locale.US));
+        return comment.get();
+    }
 
     private boolean checkId(Authentication auth, Long id) {
         String username = auth.getName();
@@ -95,48 +112,5 @@ public class CommentServiceImpl implements ICommentService {
             return username.equals(emailUserCreator) || authorityUser.equals("ADMIN");
         } else return false;
     }
-
-    @Override
-    public CommentResponseDto update(Long id, CommentRequestDto edit,String auth) {
-        UserResponseDto userResponseDto = userService.getLoggerUserData(auth);
-        if (userResponseDto.getId() != repository.getById(id).getUserId()){
-            if (userResponseDto.getRole().getName().name() != "ADMIN"){
-                throw new ForbiddenException(messageSource.getMessage("forbidden",null,Locale.US));
-            }
-
-        }
-        Optional<Comment> exists = repository.findById(id);
-        if (exists.isEmpty()){
-            throw new NotFoundException(messageSource.getMessage("comment-not-found",null, Locale.US));
-        }
-        try{
-            Comment comment = mapper.map(edit, Comment.class);
-            comment.setId(id);
-            return mapper.map(repository.save(comment), CommentResponseDto.class);
-        }catch (Exception e){
-            throw new UnableToUpdateEntityException(messageSource.getMessage("unable-to-update-comment",
-                    null, Locale.US));
-        }
-    }
-
-
-    private Comment getCommentById(Long id) {
-        Optional<Comment> comment = repository.findById(id);
-        if(comment.isEmpty()){
-            throw new NotFoundException(messageSource.getMessage("comment-not-found", null, Locale.US));
-        }
-        return comment.get();
-    }
-
-    @Override
-    public List<CommentBodyResponseDto> getAllCommentBodies() {
-        List<Comment> bodies = repository.findAllByOrderByNewsCreationDateAsc();
-        if(bodies.isEmpty()){
-            throw new EmptyListException(messageSource.getMessage("empty-list", null, Locale.US));
-        }
-        return mapper.mapAll(bodies, CommentBodyResponseDto.class);
-    }
-
-
 
 }
